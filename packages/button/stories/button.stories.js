e3789c83fc332de111098b885754627121f58b40
Rod-X <913934542@qq.com>
2022-05-21 17:39:32 +0800

    test

--- packages/button/stories/button.stories.js
+++ packages/button/stories/button.stories.js
@@ -9,7 +9,7 @@ export const demo = () => ({
     template: `<rodx-button @click="clickit">点击按钮</rodx-button>`,
     methods: {
         clickit() {
-            console.log('button');
+            console.log('button2');
         }
     }
 
