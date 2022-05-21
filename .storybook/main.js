0f96bb2d1c1afbb32f8a8c4a9a318a34a70bc1e9
Rod-X <913934542@qq.com>
2022-05-21 15:46:16 +0800

    安装storybook

---
+++ .storybook/main.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // 找到packagexia.storues.js结尾的文件作为渲染
+  stories: ['../packages/**/*.stories.js'],
+
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+};
---
+++ packages/button/index.js
@@ -0,0 +1,7 @@
+import RodxButton from './src/button.vue'
+
+RodxButton.install = Vue => {
+  Vue.component(RodxButton.name, RodxButton)
+}
+
+export default RodxButton
